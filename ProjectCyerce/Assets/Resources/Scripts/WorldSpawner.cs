21ea5297aa35bc5c7e134900fd78d894397535da
Brandan <foilpudding@gmail.com>
2019-02-11 17:39:58 -0800

    dynamic world size

--- ProjectCyerce/Assets/Resources/Scripts/WorldSpawner.cs
+++ ProjectCyerce/Assets/Resources/Scripts/WorldSpawner.cs
@@ -9,16 +9,51 @@ public class WorldSpawner : MonoBehaviour
 
     public GameObject HeroPrefab; // TEMP
 
+    public GameObject FieldPrefab;
+
+    public int MapWidth;
+    public int MapHeight;
+    public int FieldSize;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        InitWorld();
+        //InitWorld();
+        InitDynamicWorld();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InitDynamicWorld()
+    {
+        FieldSpawnerList = new List<FieldSpawner>();
+        for (int x = 0; x < MapWidth; x++)
+        {
+            for (int y = 0; y < MapHeight; y++)
+            {
+                var g = Instantiate(FieldPrefab, new Vector3(x * FieldSize, y * FieldSize, 0), transform.rotation);
+                FieldSpawnerList.Add(g.GetComponent<FieldSpawner>());
+            }
+        }
+
+        InitFieldTag(FieldSpawner.FieldTags.Start);
+        InitFieldTag(FieldSpawner.FieldTags.ShopO);
+        InitFieldTag(FieldSpawner.FieldTags.ShopD);
+        InitFieldTag(FieldSpawner.FieldTags.Dun1);
+        InitFieldTag(FieldSpawner.FieldTags.Dun2);
+        InitFieldTag(FieldSpawner.FieldTags.Dun3);
+
+        var SP = GameObject.FindWithTag("Map/StartNode").transform;
+        print("World Start POS = " + SP.position);
+        Instantiate(HeroPrefab, SP.position, SP.rotation);
+        this.transform.position = new Vector3(((MapWidth-1) * FieldSize) / 2, ((MapHeight-1) * FieldSize) / 2, 0);
+        //this.transform.localScale = new Vector3(MapWidth * FieldSize, MapHeight * FieldSize, 0);
+        GetComponent<SpriteRenderer>().size = new Vector2(MapWidth * FieldSize, MapHeight * FieldSize) * .1f;
     }
 
     public void InitWorld()
