a95355b5987ad2eba006ed64d9b4285b63efbf7e
Brandan <foilpudding@gmail.com>
2019-04-04 19:20:54 -0700

    Notification, FadeScreen

--- ProjectCyerce/Assets/Resources/Scripts/GGOBehavior.cs
+++ ProjectCyerce/Assets/Resources/Scripts/GGOBehavior.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GGOBehavior : MonoBehaviour
 {
     private GameObject WorldMap;
     private GameObject ShopMap;
     private GameObject Camera;
+    private GameObject ScreenFadeImage;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,7 @@ public class GGOBehavior : MonoBehaviour
         WorldMap = GameObject.Find("OverworldSpawner");
         ShopMap = GameObject.Find("ShopScreen");
         Camera = GameObject.Find("Main Camera");
+        ScreenFadeImage = GameObject.Find("ScreenFadeImage");
 
         ShopMap.SetActive(false);
     }
@@ -28,17 +31,37 @@ public class GGOBehavior : MonoBehaviour
 
     private Vector3 Lastposition;
 
+    public IEnumerator FadeScreen()
+    {
+        var fade = ScreenFadeImage.GetComponent<Image>();
+        int count = 30;
+        float duration = .33f;
+        Color targetColor = new Color(1, 1, 1, 1);
+        float lerp = 0.1f;
+        while(count >= 0)
+        {
+            fade.color = Color.Lerp(fade.color, targetColor, lerp);
+            count--;
+            yield return new WaitForSeconds(duration);
+        }
+        yield return null;
+    }
+
     public void TravelToShop1()
     {
+        StartCoroutine(FadeScreen());
         WorldMap.SetActive(false);
         ShopMap.SetActive(true);
 
-        var g = GameObject.FindWithTag("Map/ShopOStartNode");
-        var p = GameObject.FindWithTag("Player/player1");
+        var shopNode = GameObject.FindWithTag("Map/ShopOStartNode");
+        var player = GameObject.FindWithTag("Player/player1");
 
-        Lastposition = p.transform.position + new Vector3(0,-0.5f,0);
-        p.transform.position = g.transform.position;
-        Camera.transform.position = p.transform.position;
+        var playerRigidBody = player.GetComponent<Rigidbody2D>();
+        playerRigidBody.velocity = Vector3.zero;
+
+        Lastposition = player.transform.position + new Vector3(0,-0.5f,0);
+        player.transform.position = shopNode.transform.position;
+        Camera.transform.position = player.transform.position;
     }
 
     public void GoBackToWorld()
@@ -46,10 +69,13 @@ public class GGOBehavior : MonoBehaviour
         WorldMap.SetActive(true);
         ShopMap.SetActive(false);
 
-        var p = GameObject.FindWithTag("Player/player1");
+        var player = GameObject.FindWithTag("Player/player1");
 
-        p.transform.position = Lastposition;
-        Camera.transform.position = p.transform.position;
+        var playerRigidBody = player.GetComponent<Rigidbody2D>();
+        playerRigidBody.velocity = Vector3.zero;
+
+        player.transform.position = Lastposition;
+        Camera.transform.position = player.transform.position;
     }
     #endregion
 }
---
+++ ProjectCyerce/Assets/Resources/Scripts/NotificationManager.cs
@@ -0,0 +1,75 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class NotificationManager : MonoBehaviour {
+
+    private static string mCurrentText;
+	private Queue<string> TextQueue;
+    private Text mNotificationText;
+
+    private float mStartTime;
+    private float mDurration = 0;
+	private const float SecPerChar = 0.042647f; // 2.9 / MAX LEN
+
+	private const bool SHOW_DOOR = false;
+	private const bool SHOW_KEY = false;
+
+
+	// Use this for initialization
+	void Start () {
+        mNotificationText = GetComponent<Text>();
+       mCurrentText = string.Empty; // RESET THE MESSAGE WHENEVER NOTIFICATION MANAGER IS NEW
+        mStartTime = Time.timeSinceLevelLoad;
+		TextQueue = new Queue<string>();
+        SetNotification("Welcome Friends, Have a great game!!");
+	}
+
+    // Update is called once per frame
+    void Update () {
+        mNotificationText.text = mCurrentText;
+        if (Time.timeSinceLevelLoad - mStartTime > mDurration)
+		{
+			mCurrentText = string.Empty;
+			if(TextQueue.Count > 0)
+			{
+				mCurrentText = TextQueue.Dequeue();
+				mDurration = CalcDisplayTime(mCurrentText);
+				mStartTime = Time.timeSinceLevelLoad;
+			}
+		}
+            
+	}
+
+    public void SetNotification(string msg)
+    {
+		if(mCurrentText == string.Empty)
+		{
+			if (mCurrentText.Equals(msg))
+			{
+				print("Message Already Queued, Skiped!");
+				return;
+			}
+				
+			mCurrentText = msg;
+			mDurration = CalcDisplayTime(msg);
+			mStartTime = Time.timeSinceLevelLoad;
+		}
+		else
+		{
+			if (TextQueue.Contains(msg))
+			{
+				print("Message Already Queued, Skiped!");
+				return;
+			}
+			TextQueue.Enqueue(msg);
+		}
+    }
+
+	private float CalcDisplayTime(string msg)
+	{
+		//return Mathf.Sqrt(msg.Length) / 4.5f;// <- trying for a long enough message
+		return 2.9f;
+	}
+}
